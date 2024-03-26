683a81b2763b424e0609cac9e888b537df9c3252
NguyenHuuBien <mailgithubbien@gamil.com>
2024-03-26 15:14:42 +0700

    new Actions

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ import { adminControllers } from "./src/controllers/index.js"
 import errorHandler from "./src/controllers/errorController.js"
 import logger from "./src/config/logger.js"
 import { NotFoundError } from "./src/config/errors.js"
+import { createServer } from "http";
+import { socketHandler } from "./socket.js"
+import { Server } from "socket.io"
+import cron from "node-cron"
+import { reportOrder } from "./src/actions/ReportProductActions.js"
 
 dotenv.config()
 const app = express()
+const server = createServer(app);
+export const io = new Server(server, {
+    connectionStateRecovery: {}
+});
 app.use(morgan('combined', { stream: logger.stream.write }))
 dayjs.locale('vi')
 app.use(cors())
@@ -24,6 +33,22 @@ process.env.NODE_ENV === "production" ? mongoose.connect(process.env.MONGODB_URI
 })
 .catch(()=> {
     console.log("Database connect fail");
+})
+//socket
+socketHandler(io)
+
+//auto report
+cron.schedule("0 0 1 * *", async () => {
+    const currentDate = new Date(); // Lấy ngày hiện tại
+    const year = currentDate.getFullYear(); // Lấy năm hiện tại
+    const month = currentDate.getMonth() + 1; // Lấy tháng hiện tại (lưu ý: tháng trong JavaScript bắt đầu từ 0)
+    const startDate = new Date(year, month - 1, 1); // Ngày đầu tiên của tháng
+    const endDate = new Date(year, month, 0); // Ngày cuối cùng của tháng
+
+    await reportOrder(startDate, endDate)
+}, {
+    scheduled: true,
+    timezone: "Asia/Ho_Chi_Minh" // Chỉ định múi giờ
 })
 
 app.use('/test', (req, res) => {
@@ -41,6 +66,6 @@ process.on('uncaughtException', function (err) {
 })
 
 const PORT = process.env.PORT || 3000
-const listerner = app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running");
 })
